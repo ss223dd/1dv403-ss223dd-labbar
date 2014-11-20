2c4f96fb96e3fe048b112d4922e36da5f6ad78f5
Sebastian Ström <ss223dd@student.lnu.se>
2014-11-20 00:42:57 +0000

    1.4 Vandalen - Passes all the tests but without the error handling correctly implemented. Plus with over extensive comments which will need to  be modified upon feedback on how to resolve some sections.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,40 +1,10 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-    // The function should receive an array (persArr) with obj's as elements (obj: persons with names and their age) and return a new obj containing
-    // minAge, maxAge, averageAge and a string with the names sorted in alphabetic order.
-    
-    // Function should pass all tests by fullfilling their requirements in order to be complete.
-    
-    // Args in perArr obj's contain the 2 props: name (string of first and last name) and age (number).
-    // EX: var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-    
-    // Return obj's 4 props: minAge (number), maxAge (number), averageAge (number), 
-    // names (string of all the names of the persons, seperated with ", " and sorted in alphabetic order).
-    
-    // Test by console.log the result:
-    
-    // var makePerson = function(persArr) { var result = {};
-    // Complete the assignment
-    // return result; } 
-    // var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-    // var result = makePerson(data);
-    // console.log(result);
-    // EX: Object {minAge: 36, maxAge: 46, averageAge: 40, names: "Johan Leitet", "John Häggerud", "Mats Loock"}
-    
-    // Star-assignment: No loops (of type: for, while, do while) or conditional statements (if, switch)
-    // Implement error handling on obj's (if, switch) making sure they contain valid data, like age being an integer and the name is a string.
-
-    var names = [];
-	var ages = [];
-	var minAge = 0;
-	var maxAge = 0;
-	var ageSum = 0;
-	var averageAge = 0;
-	var result = {minAge: 0, maxAge: 0, averageAge: 0, names: ""};
 	
-	names = persArr.map(function(persArr){
+	// Uses the map method to iterate through "persArr" and, after verifying that the name is that of a valid string, 
+	// creates/returns a new array (names) containing all the name props of persArr. 
+	var names = persArr.map(function(persArr){
 	    if (typeof persArr.name !== "string"){
 	        throw new Error (persArr.name + "verkar vara felaktigt angivet då det inte kan tolkas som ett giltigt namn.");
 	    }
@@ -43,22 +13,43 @@ var makePerson = function(persArr){
 	    }
 	});
 	
-	// How to sort swe chars?
-	names.sort().join(", ");
-	
-	ages = persArr.map(function(persArr){
-	    if (persArr.age !== parseInt(persArr.age, 10)){
-	        throw new Error ("Åldern måste vara ett heltal.");
-	    }
-	    else{
-	        return persArr.age;
-	    }
+	// Sorts the names using the localeCompare function in order to sort the strings even if they're in non-ASCII characters.
+	names.sort(function(a, b){
+		return a.localeCompare(b);
 	});
 	
-	// Sort age and get min/max from array index/length or other method?
-	ages.sort();
+	// Whatever error handling/data validation I use for the ages, the tests fail. When removing - the code passes all tests... What to use?
+	var ages = persArr.map(function(persArr){
+		//1st: if (persArr.age !== parseInt(persArr.age, 10)){
+	    //         throw new Error ("Åldern måste vara ett heltal.");
+	    //2nd: if (typeof persArr.age !== "number"){
+	    //         throw new Error ("Åldern måste vara ett heltal.");
+	    //3rd: if (isNaN(persArr.age)){
+	    // 		   throw new Error ("Åldern måste vara ett heltal.");
+	    //}
+	    //else{
+	        return persArr.age;
+	    //}
+	});
 	
+	// Sums up all the values within the ages-array.
+	var ageSum = ages.reduce(function(a, b){
+		return a + b;
+	});
 	
-
+	// Self-explanatory but for clarification...
+	var result = {
+		minAge: Math.min.apply(Math, ages),
+		maxAge: Math.max.apply(Math, ages),
+		averageAge: Math.round(ageSum/ages.length),
+		names: names.join(", ") // Joins all elements in the array into one string with the specified seperator ", ".
+	};
+	
+	return result;
 };
 
+	var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+    
+    var result = makePerson(data);
+    
+    console.log(result);
